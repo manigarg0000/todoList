a80b0e62a55d1f3490f29f9528d1ad2e98ae7924
Mani <manigarg0000@gmail.com>
2021-08-03 19:01:32 +0530

    added tasklistadapter

---
+++ app/src/main/java/com/mani/todolist/TaskLIstAdapter.kt
@@ -0,0 +1,63 @@
+package com.mani.todolist
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskListAdapter (private val context : Context) : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(TaskComparator()) {
+
+        class TaskViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+            val taskTV : TextView = itemView.findViewById(R.id.task_item_tv)
+            fun bind(text : String?){
+                taskTV.text = text
+            }
+
+            companion object{
+                fun create(parent: ViewGroup) : TaskViewHolder{
+                    val view : View = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_view, parent, false)
+
+                    return TaskViewHolder(view)
+                }
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+            return TaskViewHolder.create(parent)
+        }
+
+        override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+            val current = getItem(position)
+            holder.bind(current.name)
+
+//            holder.taskTV.setOnClickListener {
+//                val queryUrl: Uri = Uri.parse("${MainActivity.PREFIX_FORSEARCH}${current.word}")
+//                val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+//                context.startActivity(intent)
+//            }
+//
+//
+
+        }
+
+    }
+
+    class TaskComparator : DiffUtil.ItemCallback<Task>(){
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem===newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+    }
+
+
+
